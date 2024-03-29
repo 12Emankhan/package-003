cf7dcc6b417f219e1f99747627b59c868ec4d1ac
12Emankhan <emankh021@gmail.com>
2024-03-29 13:30:39 -0700

    push my todo in github

---
+++ main.ts
@@ -0,0 +1,38 @@
+import inquirer from "inquirer"
+
+let todos = [];
+let condition = true;
+
+while(condition)
+{
+let addtask = await inquirer.prompt(
+    
+     [ 
+            {
+            name:'todo',
+            type: 'input',
+            message:"what you want to add in your todos"
+        },
+        {
+            name: 'addmore',
+            //for yes or no add confirm
+            type: 'confirm',
+            message:'do you want to add more?',
+            //when add confirm we must add default
+            default:'false'
+
+
+        },
+    
+        
+        
+    ]
+    
+);
+
+
+todos.push(addtask.todo);
+condition = addtask.addmore;
+console.log(todos);
+
+};
